acfd3b9d4a1bbbfff6c010fc73e7dc94f559b933
Hector Zambrano <zambranohector2002@gmail.com>
2023-05-18 10:03:27 -0500

    <feat>(<global>): <create a repository and proyect in neatbeans>
    <body>
    create repository
    add licence
    add readme
    
    add proyect with folders
    
    <footer>
    
    By HectorZL

---
+++ src/foco/Foco.java
@@ -0,0 +1,17 @@
+package foco;
+
+/**
+ *
+ * @author mrblue Hector Zambrano
+ * date 
+ */
+public class Foco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
